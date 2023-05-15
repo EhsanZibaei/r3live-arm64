48c86c1f1c342e5ca25f328ab5f8b124a6d74771
Basile Thullen <basile.thullen@heig-vd.ch>
2023-05-15 14:18:52 +0200

    put it all in the entrypoint

--- docker/ros-entrypoint.sh
+++ docker/ros-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "ROS environment setup added by ros-entrypoint.sh"
 echo "source /opt/ros/noetic/setup.bash" >> /root/.bashrc
-# echo "source /opt/realsense_build/ws_realsense/devel/setup.bash" >> /root/.bashrc
 echo "source /opt/catkin_ws/devel/setup.bash" >> /root/.bashrc
+echo "source /opt/realsense_build/ws_realsense/devel/setup.bash" >> /root/.bashrc
 exec "$@"
